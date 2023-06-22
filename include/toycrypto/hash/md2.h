8977320dc3259afb29ea64682dcae6327c48d22d
cytesys <35925878+cytesys@users.noreply.github.com>
2023-06-22 02:18:21 +0200

    Removed commented out code for the MD2 class.
    Signed-off-by: cytesys <35925878+cytesys@users.noreply.github.com>

--- include/toycrypto/hash/md2.h
+++ include/toycrypto/hash/md2.h
@@ -7,26 +7,8 @@
 #include <toycrypto/internal/hashbase.h>
 
 extern "C++" {
-//    class [[deprecated("MD2 is deprecated. See RFC 6149")]] MD2 final : public HashClass {
-//	public:
-//		TC_API MD2();
-//        TC_API ~MD2() override;
-
-//        TC_API void reset() override;
-//        TC_API void update(const char* const buffer, const size_t buflen) override;
-//        TC_API void finalize() override;
-//        TC_API void digest(unsigned char* const output, const size_t outlen) override;
-//        TC_API std::string hexdigest() override;
-
-//		TC_API static const size_t digest_size = 16;
-
-//    private:
-//        std::unique_ptr<HashImpl> pimpl;
-//	};
-
-
-class [[deprecated("MD2 is deprecated. See RFC 6149")]] MD2 final
-    : public HBase<uint8_t, 16, true> {
+    class [[deprecated("MD2 is deprecated. See RFC 6149")]] MD2 final
+        : public HBase<uint8_t, 16, true> {
     public:
         TC_API MD2();
         TC_API ~MD2() override;
